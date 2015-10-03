c7fccfde745b157e7d2e02e6c180ecab80764ea2
zhuxiuwei <jlu_zxw@126.com>
2015-10-03 11:36:51 +0800

    Add HandleDuplicate.java

---
+++ src/tools/HandleDuplicate.java
@@ -0,0 +1,138 @@
+package tools;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+/**
+ * Get rid of duplicated wechat videos. How to judge duplicate:
+ * If videos of the same day get the same file size, then consider them to be duplicate.
+ * @author xiuzhu
+ * 2015-10-3
+ */
+public class HandleDuplicate {
+
+	public String path = "";
+	private String NOTVIDEOPATH = "";
+	private String DUPLICATEDPATH = "";
+	private String LOGPATH = "";
+	
+	SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd-HH.mm.ss");
+	
+	//Constructor
+	public HandleDuplicate(String filePath){
+		//Create folder
+		this.path = filePath;
+		this.NOTVIDEOPATH = path + File.separator + "notvideo";
+		this.DUPLICATEDPATH =  path + File.separator + "duplicated";
+		this.LOGPATH = path + File.separator + "log";
+		File notVideoFolder = new File(NOTVIDEOPATH);
+		File dupeFolder = new File(DUPLICATEDPATH);
+		File logFolder = new File(LOGPATH);
+		if(!notVideoFolder.exists())
+			notVideoFolder.mkdirs();
+		if(!dupeFolder.exists())
+			dupeFolder.mkdirs();
+		if(!logFolder.exists())
+			logFolder.mkdirs();
+	}
+	
+	public void handle(){
+		System.out.println("*** Reminder: Execute RenameByModifiedTime.java before execute this. ***");
+		File sourceFolder = new File(path);
+		if (sourceFolder.isDirectory()) {
+			File files[] = sourceFolder.listFiles();
+			Map<String, String> dupMap = new HashMap<String, String>();
+			for (File file : files) {
+				if(file.isDirectory())
+					continue;
+				
+				String name = file.getName();
+				String postfix = name.substring(name.lastIndexOf(".")); 
+				
+				//if not mp4, move to "notvideo" folder
+				if(!postfix.equals(".mp4"))
+					moveFile(file, NOTVIDEOPATH);
+				
+				//handle duplicate videos
+				String day = this.getDayFromFileName(file.getName());
+				if(day.equals(""))	//Failed to extract date from file, skip this file.
+					continue;
+				String key = day + ":" + file.length();	//map key: day + file size, e.g. 20151002:202863
+				if(!dupMap.containsKey(key))
+					dupMap.put(key, name);
+				else{	//duplicate file, move to duplicated folder
+					String v = dupMap.get(key);
+					v = v + " | " + name;
+					dupMap.put(key, v);
+					moveFile(file, DUPLICATEDPATH);
+				}
+			}
+			
+			//log updates
+			logDuplications(dupMap);
+			
+		}else{
+			System.out.println(path + "不是合法的目录！");
+		}
+	}
+	
+	//Extract date from filename.
+	private String getDayFromFileName(String Filename){
+		Date date = null;
+		try {
+			date = sdf.parse(Filename);
+		} catch (ParseException e) {
+			System.out.println("Exception when convert date format: " + Filename);
+			return "";
+		}
+		String year = (1900 + date.getYear()) + "";
+		String month = (date.getMonth() + 1) < 10 ? "0" + (date.getMonth() + 1) : date.getMonth() + 1 + "";
+		String day = date.getDate() < 10 ? "0" + date.getDate(): date.getDate() + "";
+		return year + month + day;
+	}
+	
+	//Move file to another folder
+	private boolean moveFile(File file, String targetFolder){
+		System.out.println("Moving '" + file + "' to folder '" + targetFolder + "'");
+		File dest = new File(targetFolder + File.separator + file.getName());  
+		return file.renameTo(dest);
+	}
+	
+	//Log contents in dupMap
+	public void logDuplications(Map<String, String> dupMap){
+		try {
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss");
+			String logName = this.LOGPATH + File.separator + sdf.format(new Date()) + ".log";
+			FileWriter fw = new FileWriter(new File(logName));
+			Set<String> keySet = dupMap.keySet();
+			fw.write( "Date" + "\t\t\t" + "Size" + "\t\t" + "FileNames\r\n");  
+			for(String key: keySet){
+				String v = dupMap.get(key);
+				int size = v.split(" | ").length;
+				if(size > 1)
+					fw.write( key.split(":")[0] + "\t\t" + key.split(":")[1] + "\t\t" + dupMap.get(key) + "\r\n");  
+			}
+			
+			fw.flush();
+			fw.close();
+		} catch (IOException e) {
+			System.out.println("Exception when logging duplicated files:" + e.getMessage());
+		}
+	}
+	
+	public static void main(String[] args) {
+		if (args.length != 1) {
+			System.out.println("Usage: java HandleDuplicate <path>");
+			return;
+		}
+		HandleDuplicate r = new HandleDuplicate(args[0]);
+		r.handle();
+	}
+
+}
--- src/tools/RenameByModifiedTime.java
+++ src/tools/RenameByModifiedTime.java
@@ -4,57 +4,70 @@ import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
+
 /**
- * 批量修改文件名称为file modified time。
- * @author xiuzhu
- * 131228
+ * @author xiuzhu 131228
  */
 public class RenameByModifiedTime {
- 
- private HashMap<String, Integer> existedNames = new HashMap<String, Integer>();	//保存已经生成过的文件名。防止重名。
- 
- public void rename(File sourceFolder){
-  if (sourceFolder.isDirectory()){
-   String orginalNames[] = sourceFolder.list();
-   for(String s : orginalNames){
-    File f = new File(sourceFolder.getAbsolutePath() + "\\" + s);
-    if (f.isFile()) { 
-     String name = f.getName();
-     String postfix = name.substring(name.lastIndexOf("."));
-     try {
-      Date modified = new Date(f.lastModified());
-      SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd-HH.mm.ss");
-      String newName = sdf.format(modified);
-      
-      //判断重复名字
-      if(!existedNames.containsKey(newName))
-    	  existedNames.put(newName, 1);
-      else{
-    	  int count = existedNames.get(newName);
-    	  existedNames.put(newName, count + 1);
-    	  newName = newName + "_" + count;
-      }
-      
-      System.out.println(newName);
-      f.renameTo(new File(sourceFolder.getAbsolutePath() + "\\" + newName + postfix));
-     } catch (Exception e) {
-      e.printStackTrace();
-     }
-     
-    }
-   }
-  }
-  
- }
- 
- /**
-  * @param args
-  */
- public static void main(String[] args) {
-  // path是待修改名字的照片所在的目录
-  String path = "D:\\aaa";
-  File sourceFolder = new File(path);
-  new RenameByModifiedTime().rename(sourceFolder);
- }
+	private String renameType = "1"; // 1 photo or Wechat video, 2 big video
+	private String path = "";	//file source folder path
+	private HashMap<String, Integer> existedNames = new HashMap<String, Integer>();
+
+	//constructor specify path and file type
+	public RenameByModifiedTime(String filePath, String fileType){
+		this.path = filePath;
+		this.renameType = fileType;
+	}
+	
+	//rename method
+	public void rename() {
+		File sourceFolder = new File(path);
+		if (sourceFolder.isDirectory()) {
+			String orginalNames[] = sourceFolder.list();
+			for (String s : orginalNames) {
+				File f = new File(sourceFolder.getAbsolutePath() + "\\" + s);
+				if (f.isFile()) {
+					String name = f.getName();
+					String postfix = name.substring(name.lastIndexOf("."));
+					try {
+						Date modified = new Date(f.lastModified());
+						SimpleDateFormat sdf = null;
+						if (renameType.equals("2"))
+							sdf = new SimpleDateFormat("yyyyMMdd_HHmm_"); // videos
+						if (renameType.equals("1"))
+							sdf = new SimpleDateFormat("yyyy-MM-dd-HH.mm.ss"); // pictures
+						String newName = sdf.format(modified);
+						if (!existedNames.containsKey(newName))
+							existedNames.put(newName, 1);
+						else {
+							int count = existedNames.get(newName);
+							existedNames.put(newName, count + 1);
+							newName = newName + "_" + count;
+						}
+						System.out.println(name + " -> " + newName + postfix);
+						f.renameTo(new File(sourceFolder.getAbsolutePath() + "\\" + newName + postfix));
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}else{
+			System.out.println(sourceFolder + " is not a legal directory!");
+		}
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		if (args.length != 2) {
+			System.out.println("Usage: java RenameByModifiedTime <path> <FileType>。 FileType is 1 or 2, 1: picture or Wechat video, 2: video");
+			return;
+		}
+		RenameByModifiedTime r = new RenameByModifiedTime(args[0], args[1]);
+		r.rename();
+	}
 
 }
